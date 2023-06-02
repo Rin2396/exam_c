609420575a4d9b39f652c6cf93fa975df14c9b9b
Rin2396 <114080477+Rin2396@users.noreply.github.com>
2023-06-02 12:30:20 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -4,11 +4,11 @@
 int toDecimal(char str[]) {
     int number = 0, len = strlen(str);
     for (int i = 0; i < len; i++) {
-        int x = 1;
+        int n = 1;
         for (int j = 0; j < (len - 1) - i; j++) {
-            x = x * 2;
+            n = n * 2;
         }
-        number = number + str[i] * x;
+        number = number + str[i] * n;
     }
     return number;
 }
