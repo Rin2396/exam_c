fa2372394b1b9308f9942f8f25c2d26d795b1b04
Rin2396 <114080477+Rin2396@users.noreply.github.com>
2023-06-02 12:09:46 +0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct PC {
+    char* name;
+    int speed;
+	int RAM;
+};
+
+struct PC* fastestPC(struct PC data[], int n) {
+    int max_speed = data[0].speed, max_RAM = data[0].RAM;
+    char* max_name = data[0].name;
+    for (int i = 0; i < n; i++) {
+        if (data[i].speed > max_speed) {
+            max_speed = data[i].speed;
+            * max_name = data[i].name;
+            max_RAM = data[i].RAM;
+        } else if (data[i].speed == max_speed) {
+            if (data[i].RAM > max_RAM) {
+                max_speed = data[i].speed;
+                 * max_name = data[i].name;
+                max_RAM = data[i].RAM;
+            }
+        }
+    }
+}
