4ef059de636016465330cad2c0579c66edd17222
Rin2396 <114080477+Rin2396@users.noreply.github.com>
2023-06-02 12:29:52 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -2,20 +2,13 @@
 #include <string.h>
 
 int toDecimal(char str[]) {
-    int s = 0, len = strlen(str);
+    int number = 0, len = strlen(str);
     for (int i = 0; i < len; i++) {
         int x = 1;
         for (int j = 0; j < (len - 1) - i; j++) {
             x = x * 2;
         }
-        s += str[i] * x;
+        number = number + str[i] * x;
     }
-    return s;
-}
-
-
-int main() {
-    char str[6] = {1, 1, 1, 1, 1, 1};
-    printf("%d ", toDecimal(str));
-    return 0;
+    return number;
 }
